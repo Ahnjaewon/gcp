de923f8a1d6799c3a916bb905d8c6f73d63850fa
Ahnjaewon <67565707+Ahnjaewon@users.noreply.github.com>
2023-10-16 14:32:08 +0900

    Jenkins Test

--- src/main/java/com/example/server/config/WebConfig.java
+++ src/main/java/com/example/server/config/WebConfig.java
@@ -1,6 +1,6 @@
 //package com.example.server.config;
 //
-//
+
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
